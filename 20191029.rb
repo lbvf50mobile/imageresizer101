c8c541a0dff055ceb40055f62fe29067afa15869
Dmitry <lbvf50.mobile@gmail.com>
2019-10-30 18:06:35 +0700

    Change alias to start from current direcotry

--- 20191029.rb
+++ 20191029.rb
@@ -1,7 +1,7 @@
-p "alias x='ruby 20191029_Tuesday/20191029.rb'" 
+p "alias x='ruby 20191029.rb'" 
 require 'sinatra'
 require 'json'
 set :public_folder, File.dirname(__FILE__) 
 get '/' do
-    File.read('20191029_Tuesday/index.html')
+    File.read('index.html')
 end
